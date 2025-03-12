0ee582ad66ae6431fd10469025a748522c6cbdc1
guilherme-oliveira-trybe <guilherme.moliveira@hotmail.com>
2025-03-12 14:26:30 -0300

    initial commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+
+    uint256 favouriteNumber;
+    
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    mapping (string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favouriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+
+
